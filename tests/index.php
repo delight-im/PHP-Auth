78b7fb4169e1f1e36a12f0471a9a065b1cb9ad14
Marco <marco1@live.de>
2017-07-24 23:28:26 +0200

    Add warning to 'tests' explaining that files are not to be re-used

--- tests/index.php
+++ tests/index.php
@@ -6,6 +6,14 @@
  * Licensed under the MIT License (https://opensource.org/licenses/MIT)
  */
 
+/*
+ * WARNING:
+ *
+ * Do *not* use these files from the `tests` directory as the foundation
+ * for the usage of this library in your own code. Instead, please follow
+ * the `README.md` file in the root directory of this project.
+ */
+
 // enable error reporting
 error_reporting(E_ALL);
 ini_set('display_errors', 'stdout');
